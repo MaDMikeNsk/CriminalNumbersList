48f7527595a12dd42d23a97ae5c8d4dfaa110f60
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-02-09 09:41:57 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/*
+(Урок 3)Сделать детектор блатных номеров для ГИБДД (все регионы): сгенерировать список номеров и сделать метод,
+который будет проверять наличие номера в списке. Программа должна работать через консоль: запрашивать номер,
+проверять, выдавать результат проверки.
+(Урок 6)В задаче с детектором блатных номеров сделать дополнительно бинарный поиск, поиск с помощью HashSet и с помощью TreeSet.
+ Измерить и сравнить длительность 4­х видов поиска и написать результат в качестве решения домашнего задания.
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,9 +25,6 @@ public class Main {
                 "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "82", "83", "86", "87",
                 "89", "92", "94", "95"};
         ArrayList<String> criminalArray = new ArrayList<>();
-        HashSet<String> criminalHashSet = new HashSet<>();
-        TreeSet<String> criminalTreeSet = new TreeSet<>();
-        String number;
         long startTime;
         long finishTime;
 
@@ -32,71 +37,66 @@ public class Main {
                     for (String thirdLetter : lettersInNumber) {
                         for (int i = 0; i < 1000; i++) {
                             if (i <= 9) { //001..009
-                                number = firstLetter + "00" + i + secondLetter + thirdLetter + code;
-                                criminalArray.add(number);
-                                criminalHashSet.add(number);
-                                criminalTreeSet.add(number);
+                                criminalArray.add(firstLetter + "00" + i + secondLetter + thirdLetter + code);
                                 } else if (Math.floorMod(i, 10) == 0 && i < 100) { //010..090
-                                    number = firstLetter + "0" + i / 10 + "0" + secondLetter + thirdLetter + code;
-                                    criminalArray.add(number);
-                                    criminalHashSet.add(number);
-                                    criminalTreeSet.add(number);
+                                    criminalArray.add(firstLetter + "0" + i / 10 + "0" + secondLetter + thirdLetter + code);
                                 } else if (Math.floorMod(i, 111) == 0) { //111..999
-                                    number = firstLetter + i + secondLetter + thirdLetter + code;
-                                    criminalArray.add(number);
-                                    criminalHashSet.add(number);
-                                    criminalTreeSet.add(number);
+                                    criminalArray.add(firstLetter + i + secondLetter + thirdLetter + code);
                             }
                         }
                     }
                 }
             }
         }
+        HashSet<String> criminalHashSet = new HashSet<>(criminalArray);
+        TreeSet<String> criminalTreeSet = new TreeSet<>(criminalArray);
         finishTime = System.currentTimeMillis();
         System.out.println("Базы блатных номеров сформированы за " + (double)(finishTime - startTime)/1000 + " с");
         System.out.println("Количество номеров в базе: " + criminalArray.size());
+
         BufferedReader stream  = new BufferedReader(new InputStreamReader(System.in));
+        String requiredNumber;
         do {
             System.out.println("Введите номер автомобиля для поиска в базе:");
-            number = stream.readLine().trim();
-        } while (!number.matches("^[а-я][0-9]{3}[а-я]{2}[0-9]{2}$"));
+            requiredNumber = stream.readLine().trim();
+        } while (!requiredNumber.matches("^[а-я][0-9]{3}[а-я]{2}[0-9]{2}$"));
 
         // Поиск в несортированном ArrayList
         String tempNumber = "";
         boolean numberInList = false;
         int i=0;
-        startTime = System.nanoTime();
-        while (!number.equals(tempNumber) && i<criminalArray.size()){
+        startTime = System.currentTimeMillis();
+        while (!requiredNumber.equals(tempNumber) && i<criminalArray.size()){
             tempNumber = criminalArray.get(i);
-            if (number.equals(tempNumber)){
+            if (requiredNumber.equals(tempNumber)){
                 numberInList = true;
             }
             i++;
         }
-        finishTime = System.nanoTime();
-        System.out.println("Поиск в несортированном ArrayList занял: " + (double)(finishTime - startTime)/1000_000_000 + " с");
+        finishTime = System.currentTimeMillis();
+        System.out.println("Поиск в несортированном ArrayList занял: " + (finishTime - startTime) + " мс");
 
         //Поиск в отсортированном массиве
         Collections.sort(criminalArray);
         startTime = System.nanoTime();
-        int binarySearchResult = Collections.binarySearch(criminalArray, number);
+        int binarySearchResult = Collections.binarySearch(criminalArray, requiredNumber);
         finishTime = System.nanoTime();
-        System.out.println("Поиск в сортированном ArrayList занял: " + (double)(finishTime - startTime)/1000_000_000 + " с");
+        System.out.println("Поиск в сортированном ArrayList занял: " + (double)(finishTime - startTime)/1_000_000 + " мс");
 
         //Поиск в HashSet
         startTime = System.nanoTime();
-        boolean hashSetSearchResult = criminalHashSet.contains(number);
+        boolean hashSetSearchResult = criminalHashSet.contains(requiredNumber);
         finishTime = System.nanoTime();
-        System.out.println("Поиск в HashSet занял: " + (double)(finishTime - startTime)/1000_000_000 + " с");
+        System.out.println("Поиск в HashSet занял: " + (double)(finishTime - startTime)/1_000_000 + " мс");
 
         //Поиск в TreeMap
         startTime = System.nanoTime();
-        boolean treeSetSearchResult = criminalTreeSet.contains(number);
+        boolean treeSetSearchResult = criminalTreeSet.contains(requiredNumber);
         finishTime = System.nanoTime();
-        System.out.println("Поиск в TreeSet занял: " + (double)(finishTime - startTime)/1000_000_000 + " с");
+        System.out.println("Поиск в TreeSet занял: " + (double)(finishTime - startTime)/1_000_000 + " мс");
 
-        if (numberInList && binarySearchResult>0 && hashSetSearchResult && treeSetSearchResult) {
-            System.out.println("Номер " + number + " является блатным");
+        if (numberInList && binarySearchResult>=0 && hashSetSearchResult && treeSetSearchResult) {
+            System.out.println("Номер " + requiredNumber + " является блатным");
         } else {
             System.out.println("Искомого номера нет ни в одном списке блатных номеров");
         }
